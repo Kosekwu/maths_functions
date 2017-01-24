15633ef911a8cc4b9f01f40c94a2b3da34ef4511
Kosekwu Usiade <P15232404@GWHXX651WIN004W.lec-admin.dmu.ac.uk>
2017-01-24 14:28:36 +0000

    added averagex

--- math_function_div.c
+++ math_function_div.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-int div (int x, int y){
-	 return x / y;
+int div (int x, int y, int z, int a, int b){
+	 return 5 / (x + y + z + a + b);
 }
 
 int main() 
 {
 	int x;
 	int y;
-	printf( "Please input two numbers to be divided : " );
+	printf( "Please input 5 numbers to be divided : " );
 	scanf( "%i", &x );
 	scanf( "%i", &y );
+	scanf( "%i", &z );
+	scanf( "%i", &a );
+	scanf( "%i", &b );
 	printf( "The product of your two numbers is %d\n", div( x, y ) );
 		 return 0;
 }
